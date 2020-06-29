081e6ee232c02df181d488f297840a93225106e1
ALi Ansari <alidk2013@gmail.com>
2020-06-29 00:17:06 -0500

    Simple Arrya Sum problem

--- src/Hackerrank.java
+++ src/Hackerrank.java
@@ -1,2 +1,27 @@
 public class Hackerrank {
+
+    public static void main(String[] args) {
+        int[] simpleArray = {7,5,3,8,4,2,1};
+        System.out.println(Hackerrank.simpleArraySum(simpleArray));
+
+        //############################################################//
+
+
+    }
+
+//==========================================================================//
+
+    // This is "Simple Array Sum" problem
+    // This method sum all the elements of an array and return on integer value.
+    static int simpleArraySum(int[] ar) {
+        int sum = 0;
+        for (int value : ar) {
+            sum += value;
+        }
+        return sum;
+    }
+
+    /*******************************************************************/
+
+
 }
