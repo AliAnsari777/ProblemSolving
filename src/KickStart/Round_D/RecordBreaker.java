831c709a57275485049ed7fc927585728f1a1326
ALi Ansari <alidk2013@gmail.com>
2020-08-23 15:31:30 -0500

    Kick Start Round D, First problem

--- src/KickStart/Round_D/RecordBreaker.java
+++ src/KickStart/Round_D/RecordBreaker.java
@@ -5,6 +5,29 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class RecordBreaker {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+        int t = in.nextInt(); // Scanner has functions to read ints, longs, strings, chars, etc.
+        for (int i = 1; i <= t; ++i) {
+            int n = in.nextInt();
 
+            int[] list = new int[n];
+            int max = -1, result = 0;
 
+            for (int j = 0; j < n; j++) {
+                int number = in.nextInt();
+                list[j] = number;
+            }
+
+            for (int j = 0; j < n; j++) {
+                if(max < list[j] && (j+1 == n || list[j] > list[j+1])){
+                    result++;
+                }
+                if (max < list[j])
+                    max = list[j];
+            }
+
+            System.out.println("Case #" + i + ": " + result);
+        }
+    }
 }
