728bdb2fdd254b392702e56161748464029ff8c1
ALi Ansari <alidk2013@gmail.com>
2020-08-30 23:44:49 -0500

    Kangoro problem

--- src/Hackerrank.java
+++ src/Hackerrank.java
@@ -278,4 +278,20 @@ public class Hackerrank {
         }
         return score;
     }
+
+    /*******************************************************************/
+
+    // This is the "kangaroo" problem.
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+        if(v1 <= v2 ){
+            return "NO";
+        }
+
+        if(((x2 - x1) % (v2 - v1)) == 0){
+            return "YES";
+        }else{
+            return "NO";
+        }
+
+    }
 }
