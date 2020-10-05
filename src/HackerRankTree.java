d2cac0c2d9077bebd0cedf8141c3f66b85562460
ALi Ansari <alidk2013@gmail.com>
2020-10-04 23:56:24 -0700

    BST is created

--- src/HackerRankTree.java
+++ src/HackerRankTree.java
@@ -1,2 +1,42 @@
 public class HackerRankTree {
+    public static void main(String[] args) {
+
+    }
+
+    static class Node{
+        int value;
+        Node right, left;
+        public Node(){}
+
+        public Node(int value){
+            this.value = value;
+            this.right = null;
+            this.left = null;
+        }
+    }
+
+    static Node root;
+    public HackerRankTree(){
+        root = null;
+    }
+
+
+    static void insert(int value){
+        root = insertNode(root, value);
+    }
+    // average: time O(log(n)) | space O(1)
+    // Worst:   time O(n) | space O(1)
+    static Node insertNode(Node node, int value){
+        if(node == null) {
+            node = new Node(value);
+            return node;
+        }
+
+        if (value < node.value)
+            node.left = insertNode(node.left, value);
+        else if(value > node.value)
+            node.right = insertNode(node.right, value);
+
+        return node;
+    }
 }
