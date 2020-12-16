7a8fab2a097ee82885d03881e7cf36ac0c1ba06d
ALi Ansari <alidk2013@gmail.com>
2020-12-15 23:47:04 -0700

    average added to practie 2

--- src/Practice2.java
+++ src/Practice2.java
@@ -1,5 +1,30 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Practice2 {
     public static void main(String[] args) {
 
+        //############################################################//
+
+
     }
+
+    //==========================================================================//
+
+    // find a number which is in majority of array
+    int average(int[] arr){
+        HashMap<Integer, Integer> mylist = new HashMap<>();
+        for (int value : arr) {
+            if (mylist.containsKey(value)) {
+                mylist.put(value, mylist.get(value) + 1);
+            } else {
+                mylist.put(value, 1);
+            }
+        }
+        return Collections.max(mylist.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+
+    /*******************************************************************/
+
 }
