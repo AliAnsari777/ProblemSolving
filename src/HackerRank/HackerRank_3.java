970aec5c5b79fecbcea6b3a74503fbca1097710e
AliAnsari777 <alidk2013@gmail.com>
2021-12-12 15:00:55 -0700

    Forming a magic square

--- src/HackerRank/HackerRank_3.java
+++ src/HackerRank/HackerRank_3.java
@@ -44,11 +44,13 @@ public class HackerRank_3 {
 
         //############################################################//
 
-        int[] keyboards = {5};
-        int[] drives = {4};
-        System.out.println(HackerRank_3.getMoneySpent(keyboards,drives, 5));
+//        int[] keyboards = {5};
+//        int[] drives = {4};
+//        System.out.println(HackerRank_3.getMoneySpent(keyboards,drives, 5));
 
+        //############################################################//
 
+        System.out.println(HackerRank_3.catAndMouse(1,3,2));
     }
 
 
@@ -194,5 +196,30 @@ public class HackerRank_3 {
         } else {
             return "Mouse C";
         }
+    }
+
+    /*******************************************************************/
+
+    public static int formingMagicSquare(List<List<Integer>> s) {
+        int cost = Integer.MAX_VALUE;
+        int[][] t =
+                {
+                        {4,9,2,3,5,7,8,1,6},
+                        {4,3,8,9,5,1,2,7,6},
+                        {2,9,4,7,5,3,6,1,8},
+                        {2,7,6,9,5,1,4,3,8},
+                        {8,1,6,3,5,7,4,9,2},
+                        {8,3,4,1,5,9,6,7,2},
+                        {6,7,2,1,5,9,8,3,4},
+                        {6,1,8,7,5,3,2,9,4},
+                };
+        int temp = 0;
+        for(int i=0;i<8;i++){
+            temp = Math.abs(s.get(0).get(0) - t[i][0]) + Math.abs(s.get(0).get(1) - t[i][1]) + Math.abs(s.get(0).get(2) - t[i][2])
+                 + Math.abs(s.get(1).get(0) - t[i][3]) + Math.abs(s.get(1).get(1) - t[i][4]) + Math.abs(s.get(1).get(2) - t[i][5])
+                 + Math.abs(s.get(2).get(0) - t[i][6]) + Math.abs(s.get(2).get(1) - t[i][7]) + Math.abs(s.get(2).get(2) - t[i][8]);
+            cost = Math.min(temp, cost);
+        }
+        return cost;
     }
 }
