e0aec5ba78cc8c49a0f702c4dc8b7baaa912c51e
AliAnsari777 <alidk2013@gmail.com>
2021-12-31 16:49:08 -0700

    Branch Sum in BST

---
+++ src/AlgoExpert/Easy/BranchSumOfBST.java
@@ -0,0 +1,29 @@
+package AlgoExpert.Easy;
+
+import HackerRank.HackerRankTree.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BranchSumOfBST {
+
+    public List<Integer> BranchSum(Node root){
+        List<Integer> result = new ArrayList<>();
+        HelperMethod(root, 0, result);
+        return result;
+    }
+
+    private void HelperMethod(Node node, int runningSum, List<Integer> result) {
+        if (node == null)
+            return;
+
+        int newRunningSum = runningSum + node.value;
+        if (node.left == null && node.right == null){
+            result.add(newRunningSum);
+            return;
+        }
+
+        HelperMethod(node.right, newRunningSum, result);
+        HelperMethod(node.left, newRunningSum, result);
+    }
+}
