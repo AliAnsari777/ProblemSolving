06a7e1901057d0bf36f52aefe4201233b54c26bf
AliAnsari777 <alidk2013@gmail.com>
2022-02-09 21:39:52 -0700

    Encryption

---
+++ src/HackerRank/HackerRank_Medium.java
@@ -0,0 +1,27 @@
+package HackerRank;
+
+public class HackerRank_Medium {
+
+    public static void main(String[] args) {
+
+
+        //############################################################//
+
+
+    }
+
+    //==========================================================================//
+
+
+    public static String encryption(String s) {
+        String result = "test";
+
+
+
+        return result;
+    }
+
+    /*******************************************************************/
+
+
+}
