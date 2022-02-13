b27de6b67188aa9a9ea84325ffe6f3080b494db6
AliAnsari777 <alidk2013@gmail.com>
2022-02-12 23:32:02 -0700

    Encryption

--- src/HackerRank/HackerRank_Medium.java
+++ src/HackerRank/HackerRank_Medium.java
@@ -1,9 +1,13 @@
 package HackerRank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HackerRank_Medium {
 
     public static void main(String[] args) {
 
+        HackerRank_Medium.encryption("if man was meant to stay on the ground god would have given us roots");
 
         //############################################################//
 
@@ -14,7 +18,26 @@ public class HackerRank_Medium {
 
 
     public static String encryption(String s) {
-        String result = "test";
+        String result = "this is for test", temp, column;
+        List<String> grid = new ArrayList<>();
+        int row, col, size;
+
+        temp = s.replaceAll("\\s", "");;
+        size = temp.length();
+        row = (int)Math.floor(Math.sqrt(size));
+        col = (int)Math.ceil(Math.sqrt(size));;
+
+        if (row * col >= size) {
+            for (int i = col; i > 1; i--) {
+                if (temp.length() < col){
+                    grid.add(temp);
+                    continue;
+                }
+                column = temp.substring(0, col);
+                temp = temp.substring(col);
+                grid.add(column);
+            }
+        }
 
 
 
