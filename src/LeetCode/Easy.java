fbf7f3ac86df10dc55787c8f02d8d3d86d94accb
AliAnsari777 <alidk2013@gmail.com>
2022-03-30 20:26:31 -0700

    LeetCode two sum

---
+++ src/LeetCode/Easy.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Easy {
+
+    public static void main(String[] args) {
+        int[] numbers = {3,3};
+        System.out.println(Arrays.toString(Easy.twoSum(numbers, 4)));
+    }
+
+    //==========================================================================//
+
+
+    public static int[] twoSum(int[] numbers, int target){
+        HashMap<Integer, Integer> result = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            int temp = target - numbers[i];
+            if (result.containsKey(temp)){
+                return new int[]{result.get(temp), i};
+            }
+            result.put(numbers[i], i);
+        }
+        return new int[]{};
+    }
+}
