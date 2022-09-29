3851a994f035fac89542ffc2f04e1dd7d856aa61
AliAnsari777 <alidk2013@gmail.com>
2022-09-28 18:10:23 -0700

    A linked list question added to medium file of LeetCode package

---
+++ src/LeetCode/Medium.java
@@ -0,0 +1,82 @@
+package LeetCode;
+
+public class Medium {
+    public static void main(String[] args) {
+
+        ListNode one = new ListNode(2);
+        one.next = new ListNode(4);
+        one.next.next = new ListNode(3);
+
+        ListNode two = new ListNode(5);
+        two.next = new ListNode(6);
+        two.next.next = new ListNode(4);
+
+        System.out.println("Sum two linked list:");
+        ListNode result = Medium.addTwoNumbers(one, two);
+
+        while (result != null){
+            System.out.println(result.val);
+            result = result.next;
+        }
+
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        ListNode result = new ListNode();
+//        ListNode temp = result;
+//
+//        int reminder = 0, sum = 0;
+//
+//        while(l1 != null || l2 != null || reminder == 1) {
+//            sum = 0;
+//            if (l1 != null){
+//                sum += l1.val;
+//                l1 = l1.next;
+//            }
+//
+//            if (l2 != null){
+//                sum += l2.val;
+//                l2 = l2.next;
+//            }
+//
+//            sum += reminder;
+//            reminder = sum/10;
+//            ListNode node = new ListNode(sum%10);
+//            node.next = result;
+//            result = node;
+//
+//        }
+//
+//        return result;
+
+        ListNode dummy = new ListNode();
+        ListNode temp = dummy;
+        int sum=0;
+        int carry=0;
+        while(l1!=null || l2!=null || carry==1){
+            sum=0;
+            if(l1!=null){
+                sum+=l1.val;
+                l1 = l1.next;
+            }
+            if(l2!=null){
+                sum+=l2.val;
+                l2 = l2.next;
+            }
+            sum+=carry;
+            carry = sum/10;
+            ListNode node = new ListNode(sum%10);
+            temp.next = node;
+            temp = temp.next;
+        }
+        return dummy.next;
+    }
+}
