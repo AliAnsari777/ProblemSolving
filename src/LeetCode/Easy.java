d07b6b1d15639b7888ee0209ca9e1c328ec584e9
AliAnsari777 <alidk2013@gmail.com>
2022-10-24 00:27:30 -0700

    Roman to Int Enhanced version added

--- src/LeetCode/Easy.java
+++ src/LeetCode/Easy.java
@@ -2,6 +2,8 @@ package LeetCode;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
 public class Easy {
 
@@ -12,7 +14,8 @@ public class Easy {
         //############################################################//
 
         System.out.println("\nRoman to Integer");
-        System.out.println(Easy.romanToInt("IV"));
+        System.out.println(Easy.romanToInt("MCMXCII"));
+        System.out.println(Easy.romanToIntEnhanced("MCMXCII"));
 
     }
 
@@ -85,6 +88,36 @@ public class Easy {
                     result += 1000;
                     break;
             }
+        }
+
+        return result;
+    }
+
+    public static int romanToIntEnhanced(String roman) {
+        int result = 0;
+        Stack<Integer> numbers = new Stack<>();
+        Map<Character, Integer> values = new HashMap<>();
+        values.put('I', 1);
+        values.put('V', 5);
+        values.put('X', 10);
+        values.put('L', 50);
+        values.put('C', 100);
+        values.put('D', 500);
+        values.put('M', 1000);
+
+        for (int i = 0; i < roman.length(); i++) {
+            int value = values.get(roman.charAt(i));
+            if (numbers.empty() || numbers.peek() >= value){
+                numbers.add(value);
+            }else{
+                int val = value - numbers.peek();
+                numbers.pop();
+                numbers.add(val);
+            }
+        }
+
+        while (!numbers.empty()){
+            result += numbers.pop();
         }
 
         return result;
