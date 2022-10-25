c116546aa9502185f6a2590e72df9f93c31e7134
AliAnsari777 <alidk2013@gmail.com>
2022-10-24 21:34:47 -0700

    Merge sorted linked list added to the easy LeetCode class

--- src/LeetCode/Easy.java
+++ src/LeetCode/Easy.java
@@ -8,7 +8,7 @@ import java.util.Stack;
 public class Easy {
 
     public static void main(String[] args) {
-        int[] numbers = {2,7,11,15};
+        int[] numbers = {2, 7, 11, 15};
         System.out.println(Arrays.toString(Easy.twoSum(numbers, 18)));
 
         //############################################################//
@@ -17,16 +17,34 @@ public class Easy {
         System.out.println(Easy.romanToInt("MCMXCII"));
         System.out.println(Easy.romanToIntEnhanced("MCMXCII"));
 
+        //############################################################//
+
+        System.out.println("\nMerge Two sorted linked list");
+        Easy.ListNode one = new Easy.ListNode(2);
+        one.next = new Easy.ListNode(3);
+        one.next.next = new Easy.ListNode(4);
+
+        Easy.ListNode two = new Easy.ListNode(4);
+        two.next = new Easy.ListNode(5);
+        two.next.next = new Easy.ListNode(6);
+
+        Easy.ListNode result = Easy.mergeTwoLists(one, two);
+
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+
     }
 
     //==========================================================================//
 
 
-    public static int[] twoSum(int[] numbers, int target){
+    public static int[] twoSum(int[] numbers, int target) {
         HashMap<Integer, Integer> result = new HashMap<>();
         for (int i = 0; i < numbers.length; i++) {
             int temp = target - numbers[i];
-            if (result.containsKey(temp)){
+            if (result.containsKey(temp)) {
                 return new int[]{result.get(temp), i};
             }
             result.put(numbers[i], i);
@@ -41,12 +59,12 @@ public class Easy {
         int result = 0;
 
         for (int i = 0; i < romanChar.length; i++) {
-            switch (romanChar[i]){
+            switch (romanChar[i]) {
                 case 'I':
-                    if (i+1 < romanChar.length && romanChar[i + 1] == 'V') {
+                    if (i + 1 < romanChar.length && romanChar[i + 1] == 'V') {
                         result += 4;
                         i++;
-                    } else if (i+1 < romanChar.length && romanChar[i + 1] == 'X') {
+                    } else if (i + 1 < romanChar.length && romanChar[i + 1] == 'X') {
                         result += 9;
                         i++;
                     } else {
@@ -57,10 +75,10 @@ public class Easy {
                     result += 5;
                     break;
                 case 'X':
-                    if (i+1 < romanChar.length && romanChar[i + 1] == 'L') {
+                    if (i + 1 < romanChar.length && romanChar[i + 1] == 'L') {
                         result += 40;
                         i++;
-                    } else if (i+1 < romanChar.length && romanChar[i + 1] == 'C') {
+                    } else if (i + 1 < romanChar.length && romanChar[i + 1] == 'C') {
                         result += 90;
                         i++;
                     } else {
@@ -71,10 +89,10 @@ public class Easy {
                     result += 50;
                     break;
                 case 'C':
-                    if (i+1 < romanChar.length && romanChar[i + 1] == 'D') {
+                    if (i + 1 < romanChar.length && romanChar[i + 1] == 'D') {
                         result += 400;
                         i++;
-                    } else if (i+1 < romanChar.length && romanChar[i + 1] == 'M') {
+                    } else if (i + 1 < romanChar.length && romanChar[i + 1] == 'M') {
                         result += 900;
                         i++;
                     } else {
@@ -93,6 +111,8 @@ public class Easy {
         return result;
     }
 
+    /*******************************************************************/
+
     public static int romanToIntEnhanced(String roman) {
         int result = 0;
         Stack<Integer> numbers = new Stack<>();
@@ -107,20 +127,73 @@ public class Easy {
 
         for (int i = 0; i < roman.length(); i++) {
             int value = values.get(roman.charAt(i));
-            if (numbers.empty() || numbers.peek() >= value){
+            if (numbers.empty() || numbers.peek() >= value) {
                 numbers.add(value);
-            }else{
+            } else {
                 int val = value - numbers.peek();
                 numbers.pop();
                 numbers.add(val);
             }
         }
 
-        while (!numbers.empty()){
+        while (!numbers.empty()) {
             result += numbers.pop();
         }
 
         return result;
     }
 
+    /*******************************************************************/
+
+    static class ListNode {
+        int val;
+        Easy.ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, Easy.ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode result = new ListNode();
+        ListNode temp;
+        ListNode head = result;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                temp = new ListNode(list1.val);
+                list1 = list1.next;
+            } else {
+                temp = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+
+            head.next = temp;
+            head = head.next;
+
+        }
+
+        while (list1 != null) {
+            temp = new ListNode(list1.val);
+            list1 = list1.next;
+            head.next = temp;
+            head = head.next;
+        }
+
+        while (list2 != null) {
+            temp = new ListNode(list2.val);
+            list2 = list2.next;
+            head.next = temp;
+            head = head.next;
+        }
+        return result.next;
+    }
 }
