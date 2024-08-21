c86cdfc0c2ee61f02aec6563dc7c7fb0454f4d66
AliAnsari777 <alidk2013@gmail.com>
2024-08-20 22:51:57 -0700

    HackerRank Medium problems.

--- src/HackerRank/HackerRank_Medium.java
+++ src/HackerRank/HackerRank_Medium.java
@@ -1,16 +1,26 @@
 package HackerRank;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class HackerRank_Medium {
 
     public static void main(String[] args) {
 
-        System.out.println("Encryption:");
-        System.out.println(HackerRank_Medium.encryption("if man was meant to stay on the ground god would have given us roots"));
+//        System.out.println("Encryption:");
+//        System.out.println(HackerRank_Medium.encryption("if man was meant to stay on the ground god would have given us roots"));
 
         //############################################################//
+
+        List<List<Integer>> containers = new ArrayList<List<Integer>>();
+        containers.add(Arrays.asList(997612619, 934920795, 998879231, 999926463));
+        containers.add(Arrays.asList(960369681, 997828120, 999792735, 979622676));
+        containers.add(Arrays.asList(999013654, 998634077, 997988323, 958769423));
+        containers.add(Arrays.asList(997409523, 999301350, 940952923, 993020546));
+
+        System.out.println("organizingContainers:");
+        System.out.println(HackerRank_Medium.organizingContainers(containers));
 
     }
 
@@ -57,5 +67,34 @@ public class HackerRank_Medium {
 
     /*******************************************************************/
 
+    /*
+     * Complete the 'organizingContainers' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts 2D_INTEGER_ARRAY container as parameter.
+     */
 
+    public static String organizingContainers(List<List<Integer>> container) {
+        int size = container.size();
+        long[] rowSum = new long[size];
+        long[] colSum = new long[size];;
+
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++){
+                rowSum[i] += container.get(i).get(j);
+                colSum[i] += container.get(j).get(i);
+            }
+        }
+
+        Arrays.sort(rowSum);
+        Arrays.sort(colSum);
+
+        for (int i = 0; i < colSum.length; i++) {
+            if(rowSum[i] < colSum[i]) {
+                return "Impossible";
+            }
+        }
+
+        return "Possible";
+    }
 }
