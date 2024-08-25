f72a0962f01bc241b4c4bc3666ecf703e1bae9c1
AliAnsari777 <alidk2013@gmail.com>
2024-08-25 11:42:59 -0700

    LeetCode Medium problems for Amazon interview, The Longest Common Subsequence.

--- src/LeetCode/Amazon.java
+++ src/LeetCode/Amazon.java
@@ -7,13 +7,25 @@ public class Amazon {
         System.out.println(minSwaps(data));
 
         //############################################################//
+
         System.out.println("=================================================");
+        System.out.println("Longest Common Subsequence");
+        String str1 = "gtgtgatcg";
+        String str2 = "actgattag";
+        System.out.println(longestCommonSubsequence(str1, str2));
+
+        //############################################################//
+
+        System.out.println("=================================================");
+
+
 
 
     }
 
     // Given an array of 0s and 1s, find the minimum number of swaps required to make all 1s together, no matter where in the array
     // To solve this problem we used fixed sliding window
+    // Time O(N), Space O(1)
     public static int minSwaps(int[] data) {
         if(data.length <= 2)
             return 0;
@@ -36,6 +48,28 @@ public class Amazon {
         }
 
         return numOnes - maxOnes;
+    }
+
+    /*******************************************************************/
+
+    // Given two strings, find the length of the longest common subsequence
+    // To solve this problem we used dynamic programming
+    // Time O(M . N), Space O(M .N)
+    public static int longestCommonSubsequence(String text1, String text2) {
+
+        int[][] dpGrid = new int[text1.length()+1][text2.length()+1];
+
+        for (int row = text1.length()-1; row >= 0; row--) {
+            for (int col = text2.length()-1; col >= 0; col--) {
+                if(text1.charAt(row) == text2.charAt(col)){
+                    dpGrid[row][col] = 1 + dpGrid[row+1][col+1];
+                } else {
+                    dpGrid[row][col] = Math.max(dpGrid[row+1][col], dpGrid[row][col+1]);
+                }
+            }
+        }
+
+        return dpGrid[0][0];
     }
 
     /*******************************************************************/
