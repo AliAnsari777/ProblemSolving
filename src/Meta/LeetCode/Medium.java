bbee6c22f54d783169640cea4e9c3d935b009fed
AliAnsari777 <alidk2013@gmail.com>
2025-04-20 10:46:09 -0700

    LeetCode practice for the Meta Interview Day Seven

--- src/Meta/LeetCode/Medium.java
+++ src/Meta/LeetCode/Medium.java
@@ -330,6 +330,113 @@ public class Medium {
 
     /*******************************************************************/
 
+    /*
+    Given an n x n binary matrix grid, return the length of the shortest clear path in the matrix. If there is no clear path, return -1.
+
+    A clear path in a binary matrix is a path from the top-left cell (i.e., (0, 0)) to the bottom-right cell (i.e., (n - 1, n - 1)) such that:
+
+    All the visited cells of the path are 0.
+    All the adjacent cells of the path are 8-directionally connected (i.e., they are different and they share an edge or a corner).
+    The length of a clear path is the number of visited cells of this path.
+    */
+
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        if(grid[0][0] == 1 || grid[grid.length-1][grid[0].length-1] == 1)
+            return -1;
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        grid[0][0] = 1;
+        queue.add(new int[]{0, 0});
+
+        while(!queue.isEmpty()){
+            int[] cell = queue.remove();
+
+            int row = cell[0];
+            int col = cell[1];
+
+            int distance = grid[row][col];
+            if(row == grid.length-1 && col == grid[0].length-1) {
+                return distance;
+            }
+
+            for(int[] neighbour : getNeighbours(row, col, grid)){
+                int neighborRow = neighbour[0];
+                int neighbuotCol = neighbour[1];
+
+                queue.add(new int[]{neighborRow, neighbuotCol});
+                grid[neighborRow][neighbuotCol] = distance + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    private static final int[][] dir = new int[][]{{-1,-1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+
+    private List<int[]> getNeighbours(int r, int c, int[][] grid) {
+        List<int[]> neighbours = new ArrayList<>();
+
+        for(int i = 0; i < dir.length; i++) {
+            int newRow = r + dir[i][0];
+            int newCol = c + dir[i][1];
+
+            if(newRow < 0 || newCol < 0 || newRow >= grid.length || newCol >= grid[0].length || grid[newRow][newCol] != 0) {
+                continue;
+            }
+
+            neighbours.add(new int[]{newRow, newCol});
+        }
+
+        return neighbours;
+    }
+
+    /*******************************************************************/
+
+    /*
+        You are given a 0-indexed array of positive integers w where w[i] describes the weight of the ith index.
+        You need to implement the function pickIndex(), which randomly picks an index in the range [0, w.length - 1]
+        (inclusive) and returns it. The probability of picking an index i is w[i] / sum(w).
+        For example, if w = [1, 3], the probability of picking index 0 is 1 / (1 + 3) = 0.25 (i.e., 25%), and the
+        probability of picking index 1 is 3 / (1 + 3) = 0.75 (i.e., 75%).
+    */
+    class Solution {
+        private int[] prefixSums;
+        private int totalsum;
+
+        public Solution(int[] w) {
+            this.prefixSums = new int[w.length];
+            int prefixSum = 0;
+
+            for (int i = 0; i < w.length; i++) {
+                prefixSum += w[i];
+                prefixSums[i] = prefixSum;
+            }
+
+            this.totalsum = prefixSum;
+        }
+
+        public int pickIndex() {
+            double target = this.totalsum * Math.random();
+            int L = 0, R = this.prefixSums.length, M = 0;
+
+            // run a binary search to find the target zone
+            while (L < R) {
+                // better to avoid the overflow
+                M = L + (R - L) / 2;
+                if (target > this.prefixSums[M])
+                    L = M + 1;
+
+                else
+                    R = M;
+            }
+
+            return L;
+        }
+    }
+
+    /*******************************************************************/
+
+
     public class TreeNode {
       int val;
       TreeNode left;
